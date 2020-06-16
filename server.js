7e2c8e23eb933977deb45a27b14c2f9d8409ea94
Shi Sha <emailtotheowner@gmail.com>
2020-06-16 17:52:48 -0400

    path and mongojs modules imported and public static path initialized.

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ let mongodb = require("mongodb");
 let mongoose = require("mongoose");
 let db = require("./models");
 let logger = require("morgan");
+let mongojs = require("mongojs");
+let path = require("path");
 
 let app = express(); 
 let PORT = process.env.PORT || 8080;
 
 app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost/workout", { useNewUrlParser: true,  useFindAndModify: false });
 
